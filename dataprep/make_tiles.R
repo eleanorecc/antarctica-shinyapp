5241a09aa4029041317bf1f8b2b1ac33bb8286a3
Eleanore Campbell <eleanorecampbell@SRC-DWWQD7YJ64.local>
2025-11-24 23:47:26 +0100

    Refactor tile generation system for better modularity
    - Extract quantile-based tile generation logic into reusable make_tiles() function
    - Update generate_distant_tiles.R to use make_tiles() and remove redundant library calls
    - Clean up global.R: remove deprecated rast2tile(), reorganize data loading, expand study area to CCAMLR boundaries

---
+++ dataprep/make_tiles.R
@@ -0,0 +1,53 @@
+make_tiles <- function(resampled_raster, tile_directory) {
+  tryCatch({
+    ## calculate the quantiles and breaks
+    qt <- global(resampled_raster, quantile, probs = seq(0, 1, length.out = 257), na.rm = TRUE)
+    breaks <- unlist(qt)
+
+    ## reclassify the raster using the quantile breaks
+    rcm <- matrix(c(breaks[1:256], breaks[2:257], 0:255), ncol = 3)
+    rint <- classify(resampled_raster, rcm, include.lowest = TRUE, right = FALSE)
+    cols <- data.frame(value = 0:255, col = hcl.colors(256, "viridis"))
+    coltab(rint) <- cols
+    
+    ## make int1u raster
+    writeRaster(
+      rint,
+      file.path(tile_directory, "rint.tif"), 
+      datatype = "INT1U", 
+      overwrite = TRUE
+    )
+
+    ## creating color palette
+    pal <- data.frame(
+      breaks_lower = breaks[1:256],
+      breaks_upper = breaks[2:257],
+      value = 0:255,
+      col = cols$col
+    )
+    write.csv(
+      pal, 
+      file.path(tile_directory, "palette.csv"), 
+      row.names = FALSE
+    )
+
+    ## make the tiles
+    system(paste(
+      "gdal_translate -of vrt -expand rgba",
+      file.path(tile_directory, "rint.tif"),
+      file.path(tile_directory, "rint.vrt")
+    ))
+    system(paste(
+      "gdal2tiles.py -z 2-4 -w none --processes=4",
+      file.path(tile_directory, "rint.vrt"),
+      tile_directory
+    ))
+    file.remove(file.path(tile_directory, "rint.tif"))
+    file.remove(file.path(tile_directory, "rint.vrt"))
+    
+    return(TRUE)
+
+  }, error = function(e) {
+    return(FALSE)
+  })
+}
--- global.R
+++ global.R
@@ -1,34 +1,25 @@
-## concise version of what is needed for the stakeholder workshop
-
-## maps in 3 decades + recent: 1990s, 2000s, 2010s, 2020-2025
-## mean and standard deviation for each
-
-
-## use data from 3 variables:
-## (1) sea ice - extent, concentration, thickness
-## (2) primary production - ocean color and compare to cholophyll a
-## (3) salinity
-
 ## packages ----
 library(here)
 library(dplyr)
 library(tidyr)
 library(stringr)
-library(sf)
 library(shiny)
 library(bslib)
+library(shinyjs)
 library(leaflet)
 library(leaflet.extras)
 library(leaflet.minicharts)
-library(ggplot2)
+library(curl)
 library(httr2)
 library(jsonlite)
-library(curl)
+library(reticulate)
+library(sf)
 library(terra)
 library(r2d3)
-library(reticulate)
+library(ggplot2)
 
-## directories ----
+
+## directories and files ----
 dirData <- here("www")
 if(length(list.files(dirData)) == 0){
   message(
@@ -36,6 +27,8 @@ if(length(list.files(dirData)) == 0){
     check that the drive is mounted \n"
   )
 }
+ts_data <- read.csv(file.path(dirData, "tsdata.csv"))
+distant_data <-  read.csv(file.path(dirData, "distant.csv"))
 
 ## python configuration ----
 ## local: use pyenv virtualenv (for Positron GUI)
@@ -63,20 +56,22 @@ if(Sys.getenv("SHINY_PORT") == ""){
 ## boundaries of weddell gyre
 ## https://doi.org/10.15784/601652
 ## https://eos.org/editors-vox/science-in-a-frozen-ocean
-weddell_gyre_corners <- data.frame(
-  lat = c(-50, -50, -80, -80, -50),
-  lon = c(-60, 40, 40, -60, -60)
-)
+# latmin <- -80
+# latmax <- -50
+# lonmin <- -60
+# lonmax <- 40
+## expanding to boundaries of CCAMLR statistical areas
+## from Subarea 48.1 on western side to Division 58.4.4b on eastern side
+latmin <- -80
+latmax <- -45
+lonmin <- -70
+lonmax <- 60
 weddell_gyre_coords <- data.frame(
-  lat = c(
-    rep(max(weddell_gyre_corners$lat), 100),
-    rep(min(weddell_gyre_corners$lat), 100),
-    max(weddell_gyre_corners$lat)
-  ),
+  lat = c(rep(latmax, 100), rep(latmin, 100), latmax),
   lon = c(
-    seq(min(weddell_gyre_corners$lon), max(weddell_gyre_corners$lon), length.out = 100),
-    seq(max(weddell_gyre_corners$lon), min(weddell_gyre_corners$lon), length.out = 100),
-    min(weddell_gyre_corners$lon)
+    seq(lonmin, lonmax, length.out = 100),
+    seq(lonmax, lonmin, length.out = 100),
+    lonmin
   )
 )
 weddell_gyre <- weddell_gyre_coords |>
@@ -94,11 +89,30 @@ asd <- st_read(file.path(dirData, "statisticalAreasCCAMLR"))
 mgmt <- st_read(file.path(dirData, "mgmtAreas"))
 
 ## add wobecs study area on top
-# st_write(st_intersection(
-#   st_set_agr(st_transform(studyAreaWOBECbox, st_crs(4326)), "constant"),
-#   st_geometry(filter(asd, GAR_Name == "Subarea 48.6"))
-# ), file.path(dirData, "studyAreaWOBEC/WOBEC_StudyArea.shp"))
-wobec <- st_transform(st_read(file.path(dirData, "studyAreaWOBEC")), st_crs(4326))
+## make the file if it doesnt yet exist
+# latmin <- -75.54747
+# latmax <- -60.00026
+# lonmin <- -20
+# lonmax <- 9.998232
+# wobec_coords <- data.frame(
+#   lat = c(rep(latmax, 100), rep(latmin, 100), latmax),
+#   lon = c(
+#     seq(lonmin, lonmax, length.out = 100),
+#     seq(lonmax, lonmin, length.out = 100),
+#     lonmin
+#   )
+# )
+# subarea <- asd |> 
+#   filter(GAR_Name == "Subarea 48.6") |> 
+#   st_geometry()
+# wobec_coords |>
+#   st_as_sf(coords = c("lon", "lat")) |>
+#   st_combine() |>
+#   st_cast("POLYGON") |>
+#   st_set_crs(st_crs(4326)) |> 
+#   st_intersection(subarea) |> 
+#   st_write(file.path(dirData, "studyAreaWOBEC/WOBEC_StudyArea.shp"))
+wobec <- st_read(file.path(dirData, "studyAreaWOBEC"))
 
 
 ## other datasets...
@@ -113,7 +127,7 @@ wobec <- st_transform(st_read(file.path(dirData, "studyAreaWOBEC")), st_crs(4326
 
 
 
-## also extract time series for 2 areas
+## points of interest
 ## (1) kap norvega
 ## (2) maud rise sea mound
 
@@ -125,24 +139,13 @@ kap_norvegia <- data.frame(lat = -71.333332, lon = -12.2999988) |>
 
 
 ## https://www.marineregions.org/gazetteer.php?p=details&id=6990
-maud_rise_coords <- data.frame(
-  lat = c(-67.5, -63.5, -63.5, -67.5, -67.5),
-  lon = c(7.5, 7.5, -1.0, -1.0, 7.5)
-)
-maud_rise <- maud_rise_coords |>
-  st_as_sf(coords = c("lon", "lat")) |>
-  st_combine() |>
-  st_cast("POLYGON") |>
-  st_set_crs(st_crs(4326)) |>
-  st_transform(st_crs(3031))
-
 maud_rise_center <- data.frame(lat = -65.46003868, lon = 2.95221053) |>
   st_as_sf(coords = c("lon", "lat")) |>
   st_set_crs(st_crs(4326))
 
 
-## for map
-## and processing spatial data
+## for leaflet map
+## and for processing spatial data
 zooms <- 0:6
 extent <- 12367396.2185
 resolutions <- 2*extent/256/2^zooms
@@ -197,102 +200,6 @@ allrasters <- list(
     `2016-2024 minus 1998-2006` = "surfaceSalinity_Summer_20162024diff"
   )
 )
-
-
-# chla <- bind_rows(
-#   read.csv(file.path(dirData, "chlorophyllA", "timeperiod_all_months_chla.csv")) |>
-#     mutate(plot_with = "chlorophyllA") |>
-#     cbind(year = 1998:2024),
-#   read.csv(file.path(dirData, "chlorophyllA", "timeperiod_summer_months_chla.csv")) |>
-#     mutate(plot_with = "chlorophyllA_Summer") |>
-#     cbind(year = 1998:2024),
-#   read.csv(file.path(dirData, "chlorophyllA", "timeperiod_winter_months_chla.csv")) |>
-#     mutate(plot_with = "chlorophyllA_Winter") |>
-#     cbind(year = 1998:2024)) |>
-#   mutate(yaxislabel = "Chlorophyll-a (mg m^-3)")
-#
-# salinity <- bind_rows(
-#   read.csv(file.path(dirData, "surfaceSalinity", "timeperiod_all_months_salinity.csv")) |>
-#     mutate(plot_with = "surfaceSalinity") |>
-#     cbind(year = 1998:2024),
-#   read.csv(file.path(dirData, "surfaceSalinity", "timeperiod_summer_months_salinity.csv")) |>
-#     mutate(plot_with = "surfaceSalinity_Summer") |>
-#     cbind(year = 1998:2024)) |>
-#   mutate(yaxislabel = "Salinity (PSU)")
-#
-# icedays <- read.csv(file.path(dirData, "seaiceDays", "seaice_icedays.csv")) |>
-#   mutate(plot_with = "seaiceDays") |>
-#   mutate(yaxislabel = "Number of Days with Ice-Cover > 15%, Area Average") |>
-#   cbind(year = 1998:2024)
-# iceext <- read.csv(file.path(dirData, "seaiceMinExtent", "seaice_coverage_minext.csv")) |>
-#   mutate(coveragearea = coveragearea/1e6) |>
-#   pivot_longer(cols = c(index, coveragearea), values_to = "yrwgtmean") |>
-#   mutate(plot_with = "seaiceMinExtent") |>
-#   mutate(yaxislabel = ifelse(
-#     name == "coveragearea",
-#     "Area of Minimum Ice Extent (million km^2)",
-#     "Day of the Year with Minimum Ice Extent"
-#   ))
-# bind_rows(chla, salinity, icedays, iceext) |>
-#   select(plot_with, year, yvariable = yrwgtmean, yaxislabel) |>
-#   write.csv(file.path(dirData, "tsdata.csv"), row.names = FALSE)
-
-tsdata <- read.csv(file.path(dirData, "tsdata.csv"))
-
-
-rast2tile <- function(url, lyrnum, saveDir){
-  require(terra)
-  require(dplyr)
-
-  ## download the raster
-  tmptif <- file.path(saveDir, "rast0.tif")
-  download.file(url, destfile = tmptif)
-
-  ## project to sterographic south after cropping
-  r <- project(rast(tmptif, lyrs = lyrnum), "EPSG:4326") |>
-    crop(ext(c(-180, 180, -90, -50))) |>
-    project("EPSG:3031")
-
-  ## resample to match leaflet map tiles/extent
-  x <- 12367396.2185
-  template <- rast(ext(c(-x,x,-x,x)), nrow = 8192, ncol = 8192, crs = crs("EPSG:3031"))
-  rresamp <- resample(r, template)
-
-  pal <- data.frame(value = 0:255, col = hcl.colors(256, "viridis"))
-  values(rresamp) |>
-    quantile(probs = seq(0, 1, length.out = 256), na.rm = TRUE) |>
-    data.frame() |>
-    cbind(pal) |>
-    setNames(c("breaks","value","col")) |>
-    write.csv(
-      file.path(saveDir, "palette.csv"),
-      row.names = FALSE
-    )
-
-  rint <- rresamp |>
-    stretch(minq = 0.02, maxq = 0.98, minv = 0, maxv = 255) |>
-    as.int(datatype = "INT1U")
-  coltab(rint) <- pal
-
-  writeRaster(
-    rint, file.path(saveDir, "rint.tif"),
-    datatype = "INT1U",
-    overwrite = TRUE
-  )
-  system(paste(
-    "gdal_translate -of vrt -expand rgba",
-    file.path(saveDir, "rint.tif"),
-    file.path(saveDir, "rint.vrt")
-  ))
-  system(paste(
-    "gdal2tiles.py -p raster -z 2-4 -x -tmscompatible",
-    file.path(saveDir, "rint.vrt"),
-    saveDir
-  ))
-}
-
-distcsv <-  read.csv(file.path(dirData, "distAnt.csv"))
-distrasters <- as.list(pull(distcsv, name))
 
 caption_metadata <- data.frame(
   layer_pattern = c("chlorophyll", "seaice", "salinity"),
